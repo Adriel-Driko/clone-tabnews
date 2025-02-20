b32f803625e395f05855f6faa148694f94228120
Adriel-Driko <adrieldelima2005@gmail.com>
2025-02-20 13:49:39 +0000

    adiciona os arquivos e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>será se não é um teste?</h1>
+  return <h1>será se não é um teste?</h1>;
 }
 
+function teste() {
+  console.log("teste de identação");
+}
 export default Home;
